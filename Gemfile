af151be80389dacc1aa8a8be85433dafd1d4f0b1
Aquiles Valdivia <mail@aquiles.de>
2018-05-16 15:41:47 +0200

    restaurant task

--- Gemfile
+++ Gemfile
@@ -62,3 +62,13 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+
+# Rails Stylesheet
+gem 'bootstrap-sass'
+gem 'font-awesome-sass'
+gem 'simple_form'
+gem 'autoprefixer-rails'
+gem 'jquery-rails'
+
+# Faker
+gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,37 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :restaurants, only: [ :index, :show, :new, :create ] do
+    resources :reviews, only: [ :index, :new, :create ]
+  end
+
+  resources :reviews, only: [ :show ]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
+
+
+
+
+    # collection do
+    #   get '/top', to: 'restaurants#top'
+    # end
+
+    # member do
+    #   get '/chef', to:  'restaurants#chef'
+    # end
+
+
+
+
+# A visitor can see the list of all restaurants.
+# GET "restaurants"
+# He can add a new restaurant, and be redirected to the show view of that new restaurant.
+# GET "restaurants/new"
+# POST "restaurants"
+# He can see the details of a restaurant, with all the reviews related to the restaurant.
+# GET "restaurants/38"
+# He can add a new review to a restaurant
+# GET "restaurants/38/reviews/new"
+# POST "restaurants/38/reviews"
+# And that's it!
